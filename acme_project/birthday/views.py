3f6f63aa2c04f3969ac71d9e356685cffae69949
unknown <michaelmotoruf29@gmail.com>
2023-12-20 22:17:29 +0300

    21

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
@@ -9,20 +10,14 @@ from .utils import calculate_birthday_countdown
 
 class BirthdayMixin:
     model = Birthday
-    success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
+class BirthdayCreateView(BirthdayMixin, CreateView):
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
 
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
@@ -37,3 +32,12 @@ class BirthdayListView(ListView):
 
 class BirthdayDetailView(DetailView):
     model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста
+        context = super().get_context_data(**kwargs)
+        # Добавляем новый ключ
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
