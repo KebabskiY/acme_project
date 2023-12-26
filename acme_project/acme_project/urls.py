1e9b81af548eb311955658f641f053401640d80f
Gleb <michaelmotoruf29@gmail.com>
2023-12-27 00:33:42 +0300

    21

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -6,6 +6,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
+    # Подключаем urls.py приложения для работы с пользователями.
+    path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
